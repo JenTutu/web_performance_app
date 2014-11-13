66545afc8a07996c69fe053f67925d201894ba6b
Jennifer Oseitutu <JennieO23@gmail.com>
2014-11-13 13:09:49 -0600

    added a few tests and jQuery wow

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -9,23 +9,17 @@ RSpec.describe UrlsController, :type => :controller do
     end
   end
 
-  describe "GET index" do
-    it "creates an instance of Url" do
-      get :index
-      expect(@url).to exist
-    end
-  end
-
-  describe "POST index" do
-    xit "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  describe "POST create" do
+    it "creates a new url and redirects to the Url's page" do
+      post "create", :url => {:given_url => "http://google.com"}
+      expect(response).to redirect_to(assigns(:url))
     end
   end  
 
   describe "GET show" do
+    let(:url){Url.create(:given_url => "http://google.com")}
     it "returns the specified url" do
-      get :show
+      get :show, id: url
       expect(response).to render_template :show
     end
   end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Url, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:url){Url.new()}
+  context "A Url should.." do
+    it { is_expected.to respond_to(:given_url) }
+    it { is_expected.to respond_to(:page_load_time) }
+    it { is_expected.to respond_to(:num_of_links) }
+    it { is_expected.to respond_to(:status) }
+  end
+
+  context "A Url should not be valid if it doesn't include a given_url" do
+    before do
+      @url = Url.create(given_url: "")
+    end
+    it { is_expected.to_not be_valid }
+  end
 end
